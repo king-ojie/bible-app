83816e89f20d25531af8c741418ec0f054f6671a
Victor Ebhojie <victor.ebhojie@ifco.com>
2025-05-23 15:10:53 +0200

    Fixed Flask Port assignment

--- app/main.py
+++ app/main.py
@@ -42,4 +42,4 @@ def generate():
     return jsonify({"verse": verse_text, "message": sentiment_msg})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
